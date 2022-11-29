f0aec1a5c62b7c8abd73f2ba95aecadc8b3e048e
gasolinbull <kim_9dw@naver.com>
2022-11-29 21:00:32 +0900

    로그인 페이지 완성

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /**
  * 검색창 제어
  */
@@ -23,5 +25,5 @@ searchInputEl.addEventListener('blur', function () {
 /**
  * 올해가 몇 년도인지 계산
  */
- const thisYear = document.querySelector('.this-year')
- thisYear.textContent = new Date().getFullYear()
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
